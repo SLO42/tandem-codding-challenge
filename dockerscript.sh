1feed12bd7ff2643ea681037fedca93646e48ace
Samuel Oliveira <so@Samuels-Air.lan>
2020-10-31 18:15:58 -0500

    fixed docker script to use local Dockerfile

--- dockerscript.sh
+++ dockerscript.sh
@@ -1,1 +1,5 @@
-docker pull saolivei/tandem-coding-challenge:1.0 && docker run -it --rm saolivei/tandem-coding-challenge:1.0
+## for pulling without the repo do this 
+## docker pull saolivei/tandem-coding-challenge:1.0 && docker run -it --rm saolivei/tandem-coding-challenge:1.0
+
+## if you are building with this dockerfile do this:
+docker build -t tandem-coding-challenge . && docker run -it --rm tandem-coding-challenge
